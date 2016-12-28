04976b966424ce392dd4309e3947089d157ae7ae
Tadas Tamosauskas <tadas@pdfcv.com>
2016-12-28 20:07:44 +0000

    Kalvota pieva

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -4,27 +4,31 @@ using System.Collections;
 public class Cannon : MonoBehaviour {
 
 	private bool fire;
-	public float ballStartX = 0.0f;
-	public float ballStartY = 1.4f;
-	public float ballStartZ = 2.0f;
 	public float thrust = 30.0f;
 	public float cooldown = 0.5f;
 	public Transform cannonBall;
 	private float lastFiredAt;
+	private Transform cannonBallSpawn;
 
 	// Use this for initialization
 	void Start () {
 		lastFiredAt = Time.time - cooldown;
+
+		foreach(Transform child in transform){
+			if (child.tag == "CannonBallSpawn") {
+				cannonBallSpawn = child;
+				break;
+			}
+		}
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
 		fire = Input.GetButton("Fire1");
 		if (fire &&  (lastFiredAt + cooldown) <= Time.time) {
 			lastFiredAt = Time.time;
-			Vector3 startPosition = new Vector3 (transform.position.x + ballStartX, transform.position.y + ballStartY, transform.position.z + ballStartZ);
-			Transform ballObject = (Transform) Instantiate(cannonBall, startPosition, Quaternion.identity);
-			ballObject.GetComponent<Rigidbody> ().AddForce(ballObject.forward * thrust, ForceMode.VelocityChange);
+			Transform ballObject = (Transform) Instantiate(cannonBall, cannonBallSpawn.position, Quaternion.identity);
+			ballObject.GetComponent<Rigidbody> ().AddForce(cannonBallSpawn.forward * thrust, ForceMode.VelocityChange);
 		}
 	}
 }
--- Assets/Scripts/GameManagement.cs
+++ Assets/Scripts/GameManagement.cs
@@ -8,9 +8,26 @@ public class GameManagement : MonoBehaviour {
 	public int startingX = -36;
 
 	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+
 		for (int x = 0; x < numberOfFollowers / 2; x++) {
 			Instantiate(aiCharacter, new Vector3(startingX + x, 10, 30), Quaternion.identity);
 			Instantiate(aiCharacter, new Vector3(startingX + x, 10, 32), Quaternion.identity);
+		}
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+		if (Input.GetKeyDown("escape"))
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = true;
 		}
 	}
 }
